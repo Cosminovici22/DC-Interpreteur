959ef15ea1fc99ce62bd5433e4417bab1ea5fcb0
Daria Tanasie <tanasiedaria@gmail.com>
2023-05-22 13:52:49 +0300

    update?

--- DCi.c
+++ DCi.c
@@ -16,6 +16,15 @@ void trimLeft(char *instr) {
     strcpy(instr, instr + i);
 }
 
+void parseOperation(char *operation, FILE *out) {
+    for (int i = 0; operation[i]; i++) {
+        if (operation[i + 1] > 'a' && operation[i + 1] < 'z' || operation[i + 1] > 'A' && operation[i + 1] < 'Z') {
+            fprintf(out, "*");
+        }
+        fprintf(out, "%c", operation[i]);
+    }
+}
+
 int main(int argc, char *argv[]) {
     FILE *in = fopen(argv[1], "rt");
     strcat(argv[1], ".c");
@@ -85,29 +94,45 @@ int main(int argc, char *argv[]) {
                 strcpy(variables[varCount].name, p);
                 if (strchr(q, '.')) {
                     strcpy(variables[varCount].type, "float");
-                } else if (q[0] > '0' && q[0] < '9') {
+                } else if (q[0] >= '0' && q[0] <= '9') {
                     strcpy(variables[varCount].type, "int");
                 } else if (q[0] == 39) {
                     strcpy(variables[varCount].type, "char");
                 }
-                fprintf(out, "%s ", variables[varCount].type);
+                fprintf(out, "%s *%s = ", variables[varCount].type, p);
+                fprintf(out, "malloc(sizeof(%s));\n", variables[varCount].type);
             }
-            fprintf(out, "%s=%s;\n", p, q);
+            fprintf(out, "*%s =", p);
+            printf("%s", strstr(instr, q));
+            //parseOperation(strstr(instr, q), out);
             varCount++;
-        }   /*if (strcmp(p,"function") == 0) {
-                paramCount = 0;
-                p = strtok(NULL, " (");
-                fprintf(out, "void *%s(",p);
-                    if (paramCount == 0)
-                        p = strtok(NULL," )");
-                    paramCount++;
-                    p = strtok(NULL, ", ");
-                    if (paramCount == 1)
-                        fprintf(out,"void *%s", p);
-                    else {
-                        fprintf(out,", void *%s", p);
-                    }
-        }*/ else {
+        } else if (strcmp(p, "function") == 0) {
+            //paramCount = 0;
+            fprintf(out, "void *");
+            p = strtok(NULL, "()");
+            fprintf(out, "%s (", p);
+            p = strtok(NULL, "()");
+            char *q = strtok(p, " ,");
+            fprintf(out, "void *%s", p);
+            while (q != NULL) {
+                q = strtok(NULL, " ,");
+                if (q != NULL) {
+                    fprintf(out, " ,void *%s", q);
+                }
+            }
+            fprintf(out, ") {\n");
+            //printf("%s\n", p);
+            /*fprintf(out, "void *%s(",p);
+            if (paramCount == 0)
+                p = strtok(NULL," )");
+            paramCount++;
+            p = strtok(NULL, ", ");
+            if (paramCount == 1)
+                fprintf(out,"void *%s", p);
+            else {
+                fprintf(out,", void *%s", p);
+            }*/
+        } else {
             fprintf(out, "%s;\n", instr);
         }
     }
