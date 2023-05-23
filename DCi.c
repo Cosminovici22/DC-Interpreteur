7dbd4dc905ceb5eab57d56d200262a086ae3b86e
Iliescu Cosmin <iliescupetricacosmin@gmail.com>
2023-05-23 19:53:11 +0300

    vectori

--- DCi.c
+++ DCi.c
@@ -4,7 +4,7 @@
 #include <stdbool.h>
 
 typedef struct {
-    char type[5];
+    char type[10];
     char name[20];
 } TVariables;
 
@@ -16,24 +16,12 @@ void trimLeft(char *instr) {
     strcpy(instr, instr + i);
 }
 
-void parseOperation(char *operation, FILE *out) {
-    int ok = 1;
-    for (int i = 0; operation[i]; i++) {
-        if (operation[i] >= 'a' && operation[i] <= 'z' || operation[i] >= 'A' && operation[i] <= 'Z' && ok == 1) {
-            fprintf(out, "*");
-            ok = 0;
-        }
-        if (strchr(" +-*/%", operation[i])) {
-            ok = 1;
-        }
-        fprintf(out, "%c", operation[i]);
-    }
-}
-
 int main(int argc, char *argv[]) {
     FILE *in = fopen(argv[1], "rt");
     strcat(argv[1], ".c");
     FILE *out = fopen(argv[1], "wt");
+    //fpos_t posGlob;
+    //fgetpos(out, &posGlob);
 
     char matchVar[20], structVar[20] = "\0";
     TVariables *variables = (TVariables *)malloc(200*sizeof(TVariables));
@@ -76,7 +64,7 @@ int main(int argc, char *argv[]) {
         } else if (strcmp(p, "while") == 0) {
             fprintf(out, "while(%s) {\n", instr + strlen(p) + 1);
         } else if (strcmp(p, "end") == 0) {
-            fprintf(out, "}");
+            fprintf(out, "}\n");
             if(structVar[0] != '\0') {
                 fprintf(out, " %s;\n", structVar);
                 structVar[0] = '\0';
@@ -86,11 +74,16 @@ int main(int argc, char *argv[]) {
             p = strtok(NULL, " ");
             fprintf(out, "typedef struct %s {\n", p);
             strcpy(structVar, p);
+        } else if (strcmp(p, "function") == 0) {
+            p = strtok(NULL, " ");
+            fprintf(out, "void %s() {\n", p);
         } else if (strchr(instr, '=')) {
             strcpy(aux, instr);
             char *q = strtok(aux, " =");
-            q = strtok(NULL, " =(");
-            bool ok = 0;
+            q = strtok(NULL, " =");
+            bool ok = 0, arr = 0;
+            p = strtok(p, "[");
+            
             for (int i = 0; i < varCount; i++) {
                 if (strcmp(p, variables[i].name) == 0) {
                     ok = 1;
@@ -98,73 +91,69 @@ int main(int argc, char *argv[]) {
                 }
             }
             if (ok == 0) {
+                //fpos_t posCur;
+                //fgetpos(out, &posCur);
+                //fsetpos(out, &posGlob);
+                //q = strstr(q, "><");
+                printf("%s\n", q);
+                char *s;
                 strcpy(variables[varCount].name, p);
                 if (strchr(q, '.')) {
                     strcpy(variables[varCount].type, "float");
+                    fprintf(out, "%s ", variables[varCount].type);
+                } else if (strstr(q, "><") && !arr) {
+                    strcpy(variables[varCount].type, "float");
+                    char *r = strtok(q, "< >");
+                    fprintf(out, "%s %s[%s][%s];\n", variables[varCount].type, p, r, strtok(NULL, "< >"));
+                    arr = 1;
                 } else if (q[0] >= '0' && q[0] <= '9') {
                     strcpy(variables[varCount].type, "int");
+                    fprintf(out, "%s ", variables[varCount].type);
                 } else if (q[0] == 39) {
                     strcpy(variables[varCount].type, "char");
+                    fprintf(out, "%s ", variables[varCount].type);
+                } else if (strstr(q, "}{") && !arr) {
+                    printf("bau");
+                    strcpy(variables[varCount].type, "int");
+                    char *r = strtok(q, "{ }");
+                    fprintf(out, "%s %s[%s][%s];\n", variables[varCount].type, p, r, strtok(NULL, "{ }"));
+                    arr = 1;
+                } else if (strstr(q, """") && !arr) {
+                    strcpy(variables[varCount].type, "char");
+                    char *r = strtok(q, " \"");
+                    fprintf(out, "%s %s[%s][%s];\n", variables[varCount].type, p, r, strtok(NULL, " \""));
+                    arr = 1;
+                } else if (q[0] == '{' && !arr) {
+                    strcpy(variables[varCount].type, "int");
+                    fprintf(out, "%s %s[%s];\n", variables[varCount].type, p, strtok(q, "{ }"));
+                    arr = 1;
+                } else if (q[0] == '"' && !arr) {
+                    strcpy(variables[varCount].type, "char");
+                    fprintf(out, "%s %s[%s];\n", variables[varCount].type, p, strtok(q, " \""));
+                    arr = 1;
+                } else if (q[0] == '<' && !arr) {
+                    strcpy(variables[varCount].type, "float");
+                    fprintf(out, "%s %s[%s];\n", variables[varCount].type, p, strtok(q, "< >"));
+                    arr = 1;
                 }
-                fprintf(out, "%s *%s = ", variables[varCount].type, p);
-                fprintf(out, "malloc(sizeof(%s));\n", variables[varCount].type);
-            }
-            fprintf(out, "*%s = ", p);
-            char *tmp = strstr(instr, q);
-            //printf("%s ", tmp);
-            ok = 0;
-            for (int i = 0; i < varCount; i++) {
-                if (strstr(tmp, variables[i].name) != 0 && strcmp(variables[i].type, "func") != 0) {
-                    fprintf(out, "%s;\n", tmp);
-                    ok = 1;
-                    break;
+                if(!arr) {
+                    fprintf(out, "%s = %s;\n", p, strstr(instr, q));
                 }
+                //fprintf(out, "\n");
+                varCount++;
+                //fgetpos(out, &posGlob);
+                //fsetpos(out, &posCur);
+            } else {
+                fprintf(out, "%s;\n", instr);
             }
-            if (ok == 0) {
-                ///printf("%s ", variables[i].type);
-                //fprintf(out, "%s;\n", tmp);
-                parseOperation(tmp, out);
-                fprintf(out, ";\n");
-            }
-            //fprintf(out, ";\n");
-            varCount++;
-        } else if (strcmp(p, "function") == 0) {
-            //paramCount = 0;
-            fprintf(out, "void *");
-            p = strtok(NULL, "()");
-            strcpy(variables[varCount].name, p);
-            strcpy(variables[varCount].type, "func");
-            varCount++;
-            fprintf(out, "%s (", p);
-            p = strtok(NULL, "()");
-            char *q = strtok(p, " ,");
-            fprintf(out, "void *%s", p);
-            while (q != NULL) {
-                q = strtok(NULL, " ,");
-                if (q != NULL) {
-                    fprintf(out, " ,void *%s", q);
-                }
-            }
-            fprintf(out, ") {\n");
-            //printf("%s\n", p);
-            /*fprintf(out, "void *%s(",p);
-            if (paramCount == 0)
-                p = strtok(NULL," )");
-            paramCount++;
-            p = strtok(NULL, ", ");
-            if (paramCount == 1)
-                fprintf(out,"void *%s", p);
-            else {
-                fprintf(out,", void *%s", p);
-            }*/
         } else {
             fprintf(out, "%s;\n", instr);
         }
     }
-
-    for (int i = 0; i < varCount; i++) {
-        printf("\n%s %s", variables[i].name, variables[i].type);
-    }
+    
+    /*for (int i = 0; i < varCount; i++) {
+        printf("%s %s\n", variables[i].name, variables[i].type);
+    }*/
 
     fclose(in);
     fclose(out);
